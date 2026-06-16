06854d851f97aea23174dbb632ad4b4ad9f774d5
Thushanan <thushanan30@gmail.com>
2026-06-16 22:56:54 +0530

    Update for.py to uncomment countdown prints and modify output format for clarity

--- for.py
+++ for.py
@@ -1,8 +1,9 @@
-print(4,3,2,1)
-print(3,2,1)
-print(2,1)
-print(1)
+# print(4,3,2,1)
+# print(3,2,1)
+# print(2,1)
+# print(1)
 
 for i in range(4,0,-1):
     for j in range(i,0,-1):
-        print(j)
+        print(j, end=" ")
+    print()
