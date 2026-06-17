a842e2af7f69c9a45d456080672f3d51cbb0ecb9
Thushanan <thushanan30@gmail.com>
2026-06-17 22:59:50 +0530

    Comment out countdown loop in for.py and retain food item printing

--- for.py
+++ for.py
@@ -3,7 +3,13 @@
 # print(2,1)
 # print(1)
 
-for i in range(4,0,-1):
-    for j in range(i,0,-1):
-        print(j, end=" ")
-    print()
+
+# i = 6
+# for i in range(4,0,-1):
+#     for j in range(i,0,-1):
+#         print(j, end="")
+#     print()
+
+Food = ["Pizza", int(9), "Pasta", "Salad"]
+for item in Food:
+    print(item)
